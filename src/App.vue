ce832cc044ad39cfcf0c5d7218e750a86ed89f20
januarpancaran <januar352@gmail.com>
2024-10-02 13:44:27 +0700

    Fixing navbar

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,23 @@
   <fwb-navbar solid>
     <template #logo>
       <fwb-navbar-logo alt="UNNES" image-url="../src/assets/unnes.png" link="/">
-        Snake Ladder Game
+        Matematika Diskrit
       </fwb-navbar-logo>
     </template>
     <template #default="{ isShowMenu }">
       <fwb-navbar-collapse :is-show-menu="isShowMenu">
         <fwb-navbar-link link="/"> Home </fwb-navbar-link>
-        <fwb-navbar-link link="/playground"> Play </fwb-navbar-link>
         <fwb-navbar-link link="/about"> About </fwb-navbar-link>
       </fwb-navbar-collapse>
+    </template>
+    <template #right-side>
+      <router-link to="/playground">
+        <fwb-button> Play Now </fwb-button>
+      </router-link>
     </template>
   </fwb-navbar>
 </template>
 
 <script setup>
-import { FwbNavbar, FwbNavbarCollapse, FwbNavbarLink, FwbNavbarLogo } from 'flowbite-vue'
+import { FwbButton, FwbNavbar, FwbNavbarCollapse, FwbNavbarLink, FwbNavbarLogo } from 'flowbite-vue'
 </script>
