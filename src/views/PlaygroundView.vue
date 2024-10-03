17ba29d758e14c96b35705e7d7169f324faeff9e
januarpancaran <januar352@gmail.com>
2024-10-03 15:31:21 +0700

    Adding game logic

--- src/views/PlaygroundView.vue
+++ src/views/PlaygroundView.vue
@@ -1,3 +1,188 @@
 <template>
-  <h1>Play</h1>
+  <div>
+    <div id="board">
+      <div
+        v-for="(player, index) in playerProfiles"
+        :key="index"
+        :class="['playerProfiles', color[index], 'position-' + player.position]"
+      ></div>
+    </div>
+
+    <div class="player-wrapper">
+      <div class="player-list">
+        <div
+          v-for="(player, index) in playerProfiles"
+          :key="index"
+          :class="['player', color[index], { active: index === turn }]"
+        >
+          <p>Player-{{ index + 1 }}: {{ player.position }}</p>
+        </div>
+      </div>
+
+      <div class="controls flex space-x-4 mb-4">
+        <button
+          type="button"
+          class="button add-player bg-yellow-500"
+          @click="addPlayer"
+          :disabled="playerProfiles.length >= 3"
+        >
+          Add Player
+        </button>
+        <button
+          type="button"
+          class="button start-game bg-green-500"
+          @click="startGame"
+          :disabled="gameStarted"
+        >
+          Start Game
+        </button>
+        <button
+          type="button"
+          class="button reset-game bg-red-500"
+          @click="resetGame"
+          :disabled="!gameStarted"
+        >
+          Reset Game
+        </button>
+        <button
+          type="button"
+          class="button roll-dice bg-gray-500"
+          style="color: white"
+          @click="rollDice"
+          :disabled="diceDisabled"
+        >
+          Roll Dice
+        </button>
+      </div>
+    </div>
+
+    <div class="dice-wrapper">
+      <p id="dice-result">{{ diceResult }}</p>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      playerProfiles: [{ position: -1, isFinish: false }],
+      turn: 0,
+      diceRolling: false,
+      diceA: 0,
+      diceB: 0,
+      diceDisabled: true,
+      gameStarted: false,
+      color: [
+        'bg-red-500',
+        'bg-blue-500',
+        'bg-purple-500',
+        'bg-indigo-500',
+        'bg-pink-500',
+        'bg-teal-500',
+        'bg-green-500',
+        'bg-yellow-500',
+        'bg-orange-500',
+        'bg-gray-500'
+      ],
+      snakes: {
+        16: 6,
+        46: 25,
+        49: 11,
+        62: 19,
+        64: 60,
+        74: 53,
+        89: 68,
+        92: 88,
+        95: 75,
+        99: 80
+      },
+      ladders: {
+        2: 38,
+        7: 14,
+        8: 31,
+        15: 26,
+        21: 42,
+        28: 84,
+        36: 44,
+        51: 67,
+        71: 91,
+        78: 98,
+        87: 94
+      },
+      gameIsFinish: false,
+      diceAudio: new Audio('/assets/sfx/assets_sfx_roll_dice.mp3')
+    }
+  },
+  computed: {
+    diceResult() {
+      return this.diceA + this.diceB
+    }
+  },
+  methods: {
+    rollDice() {
+      this.diceRolling = true
+      this.diceA = Math.ceil(Math.random() * 6)
+      this.diceB = 0
+      this.diceAudio.play()
+      this.diceAudio.onended = () => {
+        setTimeout(() => {
+          this.diceRolling = false
+          this.movePlayer()
+        }, 1000)
+      }
+    },
+    movePlayer() {
+      const diceSum = this.diceA + this.diceB
+      let currentPlayer = this.playerProfiles[this.turn]
+
+      if (currentPlayer.position === -1) {
+        if (diceSum === 6) {
+          currentPlayer.position = 0
+        } else {
+          this.updateTurn()
+        }
+      } else {
+        currentPlayer.position += diceSum
+
+        const snakePosition = this.snakes[currentPlayer.position]
+        const ladderPosition = this.ladders[currentPlayer.position]
+
+        if (snakePosition) currentPlayer.position = snakePosition
+        if (ladderPosition) currentPlayer.position = ladderPosition
+
+        if (currentPlayer.position >= 100) {
+          currentPlayer.position = 100
+          currentPlayer.isFinish = true
+          alert(`Player-${this.turn + 1} wins!`)
+          this.updateTurn()
+        } else {
+          this.updateTurn(diceSum !== 6)
+        }
+      }
+      this.disabled = false
+    },
+    addPlayer() {
+      this.playerProfiles.push({ position: -1, isFinish: false })
+    },
+    startGame() {
+      this.diceDisabled = false
+      this.gameStarted = true
+    },
+    resetGame() {
+      this.playerProfiles = [{ position: -1, isFinish: false }]
+      this.turn = 0
+      this.diceA = 0
+      this.diceB = 0
+      this.gameStarted = false
+      this.diceDisabled = true
+    },
+    updateTurn(skipTurn = true) {
+      if (skipTurn) {
+        this.turn = (this.turn + 1) % this.playerProfiles.length
+      }
+      this.diceDisabled = false
+    }
+  }
+}
+</script>
